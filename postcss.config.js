876c315e0e2d20f21b94b0bb055b56ececea9147
chankruze <chankruze@gmail.com>
2021-11-17 17:28:17 +0530

    Add eslint and prettier
    Signed-off-by: chankruze <chankruze@gmail.com>

--- postcss.config.js
+++ postcss.config.js
@@ -8,8 +8,8 @@ Copyright (c) Geekofia 2021 and beyond
 // If you want to use other PostCSS plugins, see the following:
 // https://tailwindcss.com/docs/using-with-preprocessors
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
 };
