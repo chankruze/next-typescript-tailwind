7e98f2769ad4290beefd6c974e683a978fadbff7
chankruze <chankruze@gmail.com>
2022-12-29 00:08:20 +0530

    fix: Invalid <Link> with <a> child
    Starting with Next.js 13, <Link> renders as <a>, so attempting to use <a> as a child is invalid.
    
    Signed-off-by: chankruze <chankruze@gmail.com>

--- components/nav/NavBrand.tsx
+++ components/nav/NavBrand.tsx
@@ -10,11 +10,13 @@ import { APP_NAME } from "../../config";
 
 const NavBrand = () => {
   return (
-    <Link href="/">
-      <a className="self-center whitespace-nowrap text-2xl sm:text-3xl dark:text-white font-montserrat font-bold uppercase">
-        {APP_NAME}
-      </a>
-    </Link>
+    (<Link
+      href="/"
+      className="self-center whitespace-nowrap text-2xl sm:text-3xl dark:text-white font-montserrat font-bold uppercase">
+
+      {APP_NAME}
+
+    </Link>)
   );
 };
 
--- components/nav/NavLink.tsx
+++ components/nav/NavLink.tsx
@@ -25,25 +25,25 @@ const NavLink: FC<NavLinkProps> = ({
   closeNavbar,
 }) => {
   return (
-    <Link href={navLink.href}>
-      <a
-        className={`p-3 flex items-center border-0 lg:justify-center 
-        gap-2 font-poppins font-medium hover:text-blue-500  border-gray-200 dark:border-gray-700 
-        ${
-          isActive
-            ? "text-blue-500 dark:text-blue-400"
-            : "text-gray-800 dark:text-gray-400"
-        }`}
-        onClick={() => closeNavbar()}
-      >
-        {showIcon && <navLink.icon size={24} />}
-        {showText && (
-          <span className="uppercase font-poppins text-sm">
-            {navLink.label}
-          </span>
-        )}
-      </a>
-    </Link>
+    (<Link
+      href={navLink.href}
+      className={`p-3 flex items-center border-0 lg:justify-center 
+      gap-2 font-poppins font-medium hover:text-blue-500  border-gray-200 dark:border-gray-700 
+      ${
+        isActive
+          ? "text-blue-500 dark:text-blue-400"
+          : "text-gray-800 dark:text-gray-400"
+      }`}
+      onClick={() => closeNavbar()}>
+
+      {showIcon && <navLink.icon size={24} />}
+      {showText && (
+        <span className="uppercase font-poppins text-sm">
+          {navLink.label}
+        </span>
+      )}
+
+    </Link>)
   );
 };
 
